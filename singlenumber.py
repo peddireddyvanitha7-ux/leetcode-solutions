b47bd23c69feffea40ec4cc8c043dfa47bc8692b
peddireddyvanitha7-ux <peddireddyvanitha7@gmail.com>
2026-08-12 22:02:07 +0530

    Add LeetCode Single Number solution

---
+++ singlenumber.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        for i in range(0,len(nums),2):
+            if i==len(nums)-1:
+               return nums[i]
+            elif nums[i]!=nums[i+1]:
+                return nums[i]
+            
